2f0d7d60c1322995320fd5b147a8810149b25d5c
Your Name <you@example.com>
2021-10-01 16:10:06 +0700

    cxm

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mongoose2 = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
@@ -28,20 +29,40 @@ app.use("/", catchCookieRouter);
 //   }
 // );
 
-mongoose.connect(
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err, res) {
-    try {
-      console.log("Connected to Database");
-    } catch (err) {
-      throw err;
-    }
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log("Server connected to MongoDb!");
+  } catch (err) {
+    throw new DbConnectionError();
+    console.error(err);
   }
-);
 
-//listen port
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  "server is already done";
-});
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => {
+    "server is already done";
+  });
+};
+
+start();
+
+// mongoose.connect(
+//   "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   function (err, res) {
+//     try {
+//       console.log("Connected to Database");
+//     } catch (err) {
+//       throw err;
+//     }
+//   }
+// );
+
+// //listen port
