4dde05fe87f995f6b2f0821f55cee26f69251b51
Your Name <you@example.com>
2021-10-01 15:47:02 +0700

    ccx

--- routers/catchCookie.js
+++ routers/catchCookie.js
@@ -6,29 +6,26 @@ const router = express.Router();
 const Catch = require("../models/CatchCookie");
 
 router.get("/", async (req, res) => {
-  console.log('catch get');
-    try {
-      const catchs = await Catch.find();
-      res.json(catchs);
-    } catch (err) {
-      res.json({ message: err });
-    }
+  console.log("catch get");
+  const catchs = await Catch.find();
+  console.log(catchs);
+  res.json(catchs);
+});
+
+router.post("/", async (req, res) => {
+  console.log("catch");
+  console.log(req.body);
+
+  var { content } = req.body;
+  const hello = new Catch({
+    content,
   });
+  try {
+    const saveCatch = await hello.save();
+    res.json(saveCatch);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-  router.post("/", async (req, res) => {
-    console.log('catch');
-    console.log(req.body);
-
-    var {content} = req.body;
-    const hello = new Catch({
-      content
-    });
-    try {
-      const saveCatch = await hello.save();
-      res.json(saveCatch);
-    } catch (err) {
-      res.json({ message: err });
-    }
-  });
-
-  module.exports = router;
+module.exports = router;
